62973033b08bf232f0ae3196281947bd4794a68a
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-08-01 19:07:34 +0900

    react-beautiful-dnd를 사용하여 App 컴포넌트에 기본 드래그 앤 드롭 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
 
 function App() {
-  return <h1>Yoorello</h1>;
+  const onDragEnd = () => {};
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div>
+        <Droppable droppableId='one'>
+          {(provided) => (
+            <ul ref={provided.innerRef} {...provided.droppableProps}>
+              <Draggable draggableId='first' index={0}>
+                {(provided) => (
+                  <li ref={provided.innerRef} {...provided.draggableProps}>
+                    <span {...provided.dragHandleProps}>🍒</span>
+                  </li>
+                )}
+              </Draggable>
+              <Draggable draggableId='second' index={1}>
+                {(provided) => (
+                  <li ref={provided.innerRef} {...provided.draggableProps}>
+                    <span {...provided.dragHandleProps}>🍋</span>
+                  </li>
+                )}
+              </Draggable>
+            </ul>
+          )}
+        </Droppable>
+      </div>
+    </DragDropContext>
+  );
 }
 
 export default App;
