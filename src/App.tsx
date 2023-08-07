899a603ad5fdc878972cef2ebe27c820d4104072
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-08-07 11:48:47 +0900

    같은 보드 내 카드 이동시 로직 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import { DragDropContext, DropResult } from 'react-beautiful-dnd';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 import { useRecoilState } from 'recoil';
 import styled from 'styled-components';
 import { toDoState } from './atoms';
 import AddBoard from './Components/AddBoard';
 import Board from './Components/Board';
+import TestBoard from './Components/TestBoard';
 
 const Wrapper = styled.div`
   display: flex;
@@ -27,36 +33,67 @@ function App() {
 
   const onDragEnd = (info: DropResult) => {
     const { destination, source } = info;
+    console.log('info', info);
 
     if (!destination) return;
 
     if (source.droppableId === destination.droppableId) {
       setToDos((prevToDos) => {
-        const boardCopy = [...prevToDos[source.droppableId]];
-        const tskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, tskObj);
-        return {
-          ...prevToDos,
-          [source.droppableId]: boardCopy,
-        };
+        return prevToDos.map((board) => {
+          if (board.category === source.droppableId) {
+            const newToDos = Array.from(board.toDos);
+            const [removed] = newToDos.splice(source.index, 1);
+            newToDos.splice(destination?.index, 0, removed);
+            return {
+              ...board,
+              toDos: newToDos,
+            };
+          } else {
+            return board;
+          }
+        });
       });
     }
 
-    if (source.droppableId !== destination.droppableId) {
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        const tskObj = sourceBoard[source.index];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, tskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
+    // if (source.droppableId === destination.droppableId) {
+    //   setToDos((prevToDos) => {
+    //     return prevToDos.map((board) => {
+    //       console.log('board', board);
+    //       if (board.category === source.droppableId) {
+    //         const newToDos = Array.from(board.toDos);
+    //         const testToDos = board.toDos;
+    //         console.log('newToDos', newToDos);
+    //         console.log('testToDos', testToDos);
+
+    //         const [removed] = newToDos.splice(source.index, 1);
+    //         console.log('removed', removed);
+
+    //         newToDos.splice(destination?.index, 0, removed);
+    //         return {
+    //           ...board,
+    //           toDos: newToDos,
+    //         };
+    //       } else {
+    //         return board;
+    //       }
+    //     });
+    //   });
+    // }
+
+    // if (source.droppableId !== destination.droppableId) {
+    //   setToDos((allBoards) => {
+    //     const sourceBoard = [...allBoards[source.droppableId]];
+    //     const destinationBoard = [...allBoards[destination.droppableId]];
+    //     const tskObj = sourceBoard[source.index];
+    //     sourceBoard.splice(source.index, 1);
+    //     destinationBoard.splice(destination?.index, 0, tskObj);
+    //     return {
+    //       ...allBoards,
+    //       [source.droppableId]: sourceBoard,
+    //       [destination.droppableId]: destinationBoard,
+    //     };
+    //   });
+    // }
   };
 
   return (
@@ -64,8 +101,13 @@ function App() {
       <AddBoard />
       <Wrapper>
         <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board key={boardId} boardId={boardId} toDos={toDos[boardId]} />
+          {toDos.map((toDo) => (
+            <TestBoard
+              key={toDo.id}
+              boardId={toDo.id}
+              toDos={toDo.toDos}
+              category={toDo.category}
+            />
           ))}
         </Boards>
       </Wrapper>
