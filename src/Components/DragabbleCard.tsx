4b16d0669ec0053a894966fa3853e26d3013c972
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-08-07 15:12:54 +0900

    to do 삭제 기능

--- src/Components/DragabbleCard.tsx
+++ src/Components/DragabbleCard.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import { useSetRecoilState } from 'recoil';
 import styled from 'styled-components';
+import { toDoState } from '../atoms';
+import { AiOutlineDelete } from 'react-icons/ai';
 
 const Card = styled.div<{ $isDragging: boolean }>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   border-radius: 5px;
   margin-bottom: 5px;
   padding: 10px 10px;
@@ -12,6 +18,18 @@ const Card = styled.div<{ $isDragging: boolean }>`
     props.$isDragging ? '0px 5px 25px #b0b0b0' : 'none'};
 `;
 
+const Button = styled.button`
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+  transition: transform 0.2s ease;
+
+  &:hover {
+    color: #ff7458;
+    transform: scale(1.2);
+  }
+`;
+
 interface IDragabbleCardProps {
   toDoId: string;
   toDoText: string;
@@ -19,6 +37,21 @@ interface IDragabbleCardProps {
 }
 
 function DragabbleCard({ toDoId, toDoText, index }: IDragabbleCardProps) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onDelete = (toDoId: string) => {
+    if (window.confirm(`${toDoText} 할 일을 삭제하시겠습니까?`)) {
+      setToDos((prevToDos) => {
+        return prevToDos.map((board) => {
+          const updatedToDos = board.toDos.filter((todo) => todo.id !== toDoId);
+          return {
+            ...board,
+            toDos: updatedToDos,
+          };
+        });
+      });
+    }
+  };
+
   return (
     <Draggable draggableId={toDoId + ''} index={index}>
       {(provided, snapshot) => (
@@ -28,7 +61,10 @@ function DragabbleCard({ toDoId, toDoText, index }: IDragabbleCardProps) {
           {...provided.dragHandleProps}
           {...provided.draggableProps}
         >
-          {toDoText}
+          <span>{toDoText}</span>
+          <Button onClick={() => onDelete(toDoId)}>
+            <AiOutlineDelete name='delete' />
+          </Button>
         </Card>
       )}
     </Draggable>
