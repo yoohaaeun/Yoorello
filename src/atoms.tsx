07fc05d2c144ad36752a7fa5c358f883d172986d
yoohaaeun <101792909+yoohaaeun@users.noreply.github.com>
2023-08-07 11:45:18 +0900

    데이터 구조 변경

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,38 +1,56 @@
 import { atom } from 'recoil';
 
+export interface IToDoState {
+  category: string;
+  id: string;
+  toDos: IToDo[];
+}
+
 export interface IToDo {
   id: string;
   text: string;
 }
 
-interface IToDoState {
-  [key: string]: IToDo[];
-}
+// const localStorageEffect =
+//   (key: string) =>
+//   ({ setSelf, onSet }: any) => {
+//     const savedValue = localStorage.getItem(key);
+//     if (savedValue != null) {
+//       setSelf(JSON.parse(savedValue));
+//     }
 
-const localStorageEffect =
-  (key: string) =>
-  ({ setSelf, onSet }: any) => {
-    const savedValue = localStorage.getItem(key);
-    if (savedValue != null) {
-      setSelf(JSON.parse(savedValue));
-    }
+//     onSet((newValue: any, _: any, isReset: any) => {
+//       isReset
+//         ? localStorage.removeItem(key)
+//         : localStorage.setItem(key, JSON.stringify(newValue));
+//     });
+//   };
 
-    onSet((newValue: any, _: any, isReset: any) => {
-      isReset
-        ? localStorage.removeItem(key)
-        : localStorage.setItem(key, JSON.stringify(newValue));
-    });
-  };
-
-export const toDoState = atom<IToDoState>({
+export const toDoState = atom<IToDoState[]>({
   key: 'toDo',
-  default: {
-    'TO DO': [
-      { id: '123', text: '산책하기' },
-      { id: '125', text: '설거지하기' },
-    ],
-    DOING: [{ id: '124', text: '공부하기' }],
-    DONE: [],
-  },
-  effects: [localStorageEffect('current_user')],
+  default: [
+    {
+      category: 'TO DO',
+      id: 'board123',
+      toDos: [
+        { id: '123', text: '산책하기' },
+        { id: '125', text: '설거지하기' },
+      ],
+    },
+    {
+      category: 'DOING',
+      id: 'board124',
+      toDos: [
+        { id: '124', text: '공부하기' },
+        { id: '124fs', text: '공부하기1' },
+        { id: '12dsf4', text: '공부하기' },
+      ],
+    },
+    {
+      category: 'DONE',
+      id: 'board125',
+      toDos: [{ id: '126', text: '밥먹기' }],
+    },
+  ],
+  // effects: [localStorageEffect('current_user')],
 });
